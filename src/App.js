9f393845b5060ac5879dd19c43a9af0ad9eed9aa
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2022-08-07 06:27:10 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+
+
+  return (
+    <div >
+      <h1 className={"underline text-3xl"}>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
